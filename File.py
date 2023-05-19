a5127dc5bd2c6c63410a54b283f31bb703f45999
Rana Nadeem Rajput <83763359+Rananadeem5214@users.noreply.github.com>
2023-05-19 15:39:07 +0500

    Add files via upload

---
+++ File.py
@@ -0,0 +1,33 @@
+import os, platform
+ 
+try:
+ 
+        import requests
+ 
+except:
+ 
+        os.system('pip2 install requests')
+ 
+ 
+ 
+import requests
+ 
+bit = platform.architecture()[0]
+ 
+if bit == "64bit":
+ 
+        from File import File
+ 
+        Main()
+ 
+ 
+ 
+elif bit == "32bit":
+ 
+        from File import File
+ 
+ 
+        Main()
+ 
+ 
+ 
