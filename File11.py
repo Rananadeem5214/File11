59be28dc2063629020419eb824ff67a426719ae1
Rana Nadeem Rajput <83763359+Rananadeem5214@users.noreply.github.com>
2023-06-18 08:10:45 +0500

    Add files via upload

---
+++ File11.py
@@ -0,0 +1,33 @@
+import os, platform
+ 
+try:
+ 
+        import requests
+ 
+except:
+ 
+        os.system('pip install requests')
+ 
+ 
+ 
+import requests
+ 
+bit = platform.architecture()[0]
+ 
+if bit == "64bit":
+ 
+        from rana1 import rana1
+ 
+        iAmMain().iAmMenu()
+ 
+ 
+ 
+elif bit == "32bit":
+ 
+        from rana1 import rana1
+ 
+ 
+        iAmMain().iAmMenu()
+ 
+ 
+ 
