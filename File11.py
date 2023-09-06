62f78015a0f40479c2dc047296b9566c04ba0d86
Rana Nadeem Rajput <83763359+Rananadeem5214@users.noreply.github.com>
2023-09-06 10:31:55 +0500

    Add files via upload

--- File11.py
+++ File11.py
@@ -1,1 +1,10 @@
-import rana1
+import os,platform
+os.system('git pull')
+print('\033[1;32m [•] Join Rana Whatsapp Group')
+os.system('xdg-open https://chat.whatsapp.com/GWTPaJVAy1gDHIDgHEw0Ht')
+ 
+trt=platform.architecture()[0]
+if trt=="32bit":
+    __import__("rana1")
+elif trt=="64bit":
+    __import__("rana1")
