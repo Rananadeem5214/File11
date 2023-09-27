dca29a8db218464774ecc309a398b8d96105ec53
Rana Nadeem Rajput <83763359+Rananadeem5214@users.noreply.github.com>
2023-09-27 07:34:32 +0500

    Update File11.py

--- File11.py
+++ File11.py
@@ -1,13 +1,11 @@
 import os,platform
 print(f'\x1b[1;92m[√] PLEASE WAIT CHECKING UPDATE...')
 os.system('git pull')
-os.system('pip uninstall requests chardet urllib3 idna certifi -y;pip install chardet urllib3 idna certifi requests')
-os.system("pkg install espeak") 
 print('\033[1;32m [•] Join Rana Whatsapp Group')
 os.system('xdg-open https://chat.whatsapp.com/GWTPaJVAy1gDHIDgHEw0Ht')
  
-trt=platform.architecture()[0]
-if trt=="32bit":
+rana=platform.architecture()[0]
+if rana=="32bit":
     __import__("rana1")
-elif trt=="64bit":
+elif rana=="64bit":
     __import__("rana1")
