1c21f781100cf3167c5394a7e4689a1556448292
Rana Nadeem Rajput <83763359+Rananadeem5214@users.noreply.github.com>
2023-10-02 09:00:21 +0500

    Update File11.py

--- File11.py
+++ File11.py
@@ -1,7 +1,6 @@
 import os,platform
 print(f'\x1b[1;92m[√] PLEASE WAIT CHECKING UPDATE...')
 os.system('git pull')
-os.system('pip install pycurl')
 print('\033[1;32m [•] JOIN RANA WhATSAP GROUP')
 os.system('xdg-open https://chat.whatsapp.com/GWTPaJVAy1gDHIDgHEw0Ht')
  
