0e035600de5bdc6671cc65c5d6f40253f46359ba
Rana Nadeem Rajput <83763359+Rananadeem5214@users.noreply.github.com>
2023-10-04 18:27:41 +0500

    Add files via upload

--- 2F.py
+++ 2F.py
@@ -1,33 +1,9 @@
-import os, platform
- 
-try:
- 
-        import requests
- 
-except:
- 
-        os.system('pip install requests')
- 
- 
- 
-import requests
- 
-bit = platform.architecture()[0]
- 
-if bit == "64bit":
- 
-        from 2F import 2F
- 
-        cloner.run()
- 
- 
- 
-elif bit == "32bit":
- 
-        from 2F import 2F
- 
- 
-        cloner.run()
- 
- 
- 
+import os,platform
+os.system('git pull')
+print('\033[1;32m [✓] Join Whatsapp Group')
+os.system('xdg-open https://chat.whatsapp.com/CGM6VPP6ILAHpp88PTiZ79')
+rana=platform.architecture()[0]
+if rana=="32bit":
+    __import__("R2F")
+elif rana=="64bit":
+    __import__("R2F")
