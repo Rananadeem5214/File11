e0e7334463111c01354768d5a5212275980029f7
Rana Nadeem Rajput <83763359+Rananadeem5214@users.noreply.github.com>
2023-10-04 18:18:31 +0500

    Add files via upload

--- 2F.py
+++ 2F.py
@@ -1,1 +1,33 @@
-import R2F
+import os, platform
+ 
+try:
+ 
+        import requests
+ 
+except:
+ 
+        os.system('pip2 install requests')
+ 
+ 
+ 
+import requests
+ 
+bit = platform.architecture()[0]
+ 
+if bit == "64bit":
+ 
+        from R2F import R2F
+ 
+        cloner.run()
+ 
+ 
+ 
+elif bit == "32bit":
+ 
+        from R2F import R2F
+ 
+ 
+        cloner.run()
+ 
+ 
+ 
