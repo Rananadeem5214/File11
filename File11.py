939ad889613718cbd0e8c6f37735b24c605e845c
Rana Nadeem Rajput <83763359+Rananadeem5214@users.noreply.github.com>
2023-10-05 08:05:11 +0500

    Update File11.py

--- File11.py
+++ File11.py
@@ -1,11 +1,8 @@
 import os,platform
 print(f'\x1b[1;92m[√] PLEASE WAIT CHECKING UPDATE...')
 os.system('git pull')
-os.system("pip uninstall urllib3 requests chardet idna certifi -y");os.system("pip install urllib3 requests chardet idna certifi")
-os.system('clear')
 print('\033[1;32m [•] JOIN RANA WhATSAP GROUP')
 os.system('xdg-open https://chat.whatsapp.com/GWTPaJVAy1gDHIDgHEw0Ht')
-os.system('clear')
 rana=platform.architecture()[0]
 if rana=="32bit":
     __import__("rana")
