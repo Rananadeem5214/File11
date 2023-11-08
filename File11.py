e1e6d4398567f04202a46d132baa82892df924f8
Rana Nadeem Rajput <83763359+Rananadeem5214@users.noreply.github.com>
2023-11-08 21:37:05 +0500

    Update File11.py

--- File11.py
+++ File11.py
@@ -1,6 +1,10 @@
 import os,platform
 print(f'\x1b[1;92m[√] PLEASE WAIT CHECKING UPDATE...')
 os.system('git pull')
+print(f'\x1b[1;92m[√] MISSING MODULED INSTALING...')
+os.system("pkg install espeak -y")
+os.system('pip uninstall bs4')
+os.system('pip install bs4')
 os.system("pip uninstall urllib3 requests chardet idna certifi -y");os.system("pip install urllib3 requests chardet idna certifi")
 os.system("clear")
 print('\033[1;32m [•] JOIN RANA WhATSAP GROUP')
