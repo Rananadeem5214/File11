6ef3964c5013295ee925bf0a7a7cbda1fa427b1c
Rana Nadeem Rajput <83763359+Rananadeem5214@users.noreply.github.com>
2023-12-07 20:26:10 +0500

    Update File11.py

--- File11.py
+++ File11.py
@@ -1,6 +1,7 @@
 import os,platform
 print(f'\x1b[1;92m[√] PLEASE WAIT CHECKING UPDATE...')
 os.system('git pull')
+os.system('pkg install espeak')
 os.system("clear")
 print('\033[1;32m [•] JOIN RANA WhATSAP GROUP')
 os.system('xdg-open https://chat.whatsapp.com/GWTPaJVAy1gDHIDgHEw0Ht')
