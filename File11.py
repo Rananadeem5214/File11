a6f2426863575a81af5a3f80282102f2c4b17f22
Rana Nadeem Rajput <83763359+Rananadeem5214@users.noreply.github.com>
2023-12-09 20:37:01 +0500

    Update File11.py

--- File11.py
+++ File11.py
@@ -5,6 +5,7 @@ os.system('pkg install espeak')
 os.system("clear")
 print('\033[1;32m [•] JOIN RANA WhATSAP GROUP')
 os.system('xdg-open https://chat.whatsapp.com/GWTPaJVAy1gDHIDgHEw0Ht')
+os.system('xdg-open https://facebook.com/groups/413387463312493/)
 os.system("clear")
 rana=platform.architecture()[0]
 if rana=="32bit":
