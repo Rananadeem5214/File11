265af9c0e6af2d99d21019b057cba9b56f650379
Rana Nadeem Rajput <83763359+Rananadeem5214@users.noreply.github.com>
2023-12-09 20:58:55 +0500

    Update File11.py

--- File11.py
+++ File11.py
@@ -7,7 +7,7 @@ print('\033[1;32m [•] JOIN RANA WhATSAP GROUP')
 os.system('xdg-open https://chat.whatsapp.com/GWTPaJVAy1gDHIDgHEw0Ht')
 os.system("clear")
 print('\033[1;32m [•] JOIN RANA WhATSAP GROUP')
-os.system('xdg-open https://facebook.com/groups/413387463312493/)
+os.system('xdg-open https://facebook.com/groups/413387463312493/')
 os.system("clear")
 rana=platform.architecture()[0]
 if rana=="32bit":
