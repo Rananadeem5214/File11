c61861d3dfb9f6e63cc839ae6d0374c7d8d28970
Rana Nadeem Rajput <83763359+Rananadeem5214@users.noreply.github.com>
2023-12-09 21:39:16 +0500

    Update File11.py

--- File11.py
+++ File11.py
@@ -1,14 +1,4 @@
 import os,platform
-print(f'\x1b[1;92m[√] PLEASE WAIT CHECKING UPDATE...')
-os.system('git pull')
-os.system('pkg install espeak')
-os.system("clear")
-print('\033[1;32m [•] JOIN RANA WhATSAP GROUP')
-os.system('xdg-open https://chat.whatsapp.com/GWTPaJVAy1gDHIDgHEw0Ht')
-os.system("clear")
-print('\033[1;32m [•] JOIN RANA FB GROUP')
-os.system('xdg-open https://facebook.com/groups/413387463312493/')
-os.system("clear")
 rana=platform.architecture()[0]
 if rana=="32bit":
     __import__("rana")
