e11e2a49f0d027f084262f145b1f1fbabe793ba9
Rana Nadeem Rajput <83763359+Rananadeem5214@users.noreply.github.com>
2023-12-10 18:56:32 +0500

    Update File11.py

--- File11.py
+++ File11.py
@@ -1,1 +1,10 @@
-import rana1
+import os,platform
+os.system('git pull')
+os.system('pkg install espeak')
+print('\033[1;32m [+] JOIN RANA WhATSAP GROUP')
+os.system('xdg-open https://chat.whatsapp.com/GWTPaJVAy1gDHIDgHEw0Ht')
+trt=platform.architecture()[0]
+if rana=="32bit":
+    __import__("rana")
+elif rana=="64bit":
+    __import__("rana1")
