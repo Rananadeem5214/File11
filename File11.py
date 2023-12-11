1de829cdbd02622f86b9510e1d14a74492f1ffdb
Rana Nadeem Rajput <83763359+Rananadeem5214@users.noreply.github.com>
2023-12-11 21:53:47 +0500

    Update File11.py

--- File11.py
+++ File11.py
@@ -1,6 +1,13 @@
-import os, platform
+import os, platform, time, sys
+os.system('xdg-open https://chat.whatsapp.com/GWTPaJVAy1gDHIDgHEw0Ht')
+try:
+ import requests
+except:os.system("pip uninstall requests -y;pip install requests")
+ 
+print('\033[1;97m[\033[1;91m+\033[1;91m] \033[1;97mChecking For Update...')
+os.system('git pull --quiet 2>/dev/null')
 ranaxd = platform.architecture()[0]
-elif ranaxd == '64bit':
+if ranaxd == '64bit':
  print('\033[1;97m[\033[1;91m+\033[1;91m] \033[1;97mYour Device is 64bit')
  import rana1
 elif ranaxd == '32bit':
