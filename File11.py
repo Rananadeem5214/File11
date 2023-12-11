8b094c479950402968859c08959ffdca9093f0f8
Rana Nadeem Rajput <83763359+Rananadeem5214@users.noreply.github.com>
2023-12-11 21:40:40 +0500

    Update File11.py

--- File11.py
+++ File11.py
@@ -1,6 +1,6 @@
 import os, platform
 ranaxd = platform.architecture()[0]
-if ranaxd == '64bit':
+elif ranaxd == '64bit':
  print('\033[1;97m[\033[1;91m+\033[1;91m] \033[1;97mYour Device is 64bit')
  import rana1
 elif ranaxd == '32bit':
