7360d668acf0d3d9ada31bcf41d8bbb771147c58
Tamerzizenn <165315946+Tamerzizenn@users.noreply.github.com>
2024-09-19 12:53:06 +0100

    Update File11.py

--- File11.py
+++ File11.py
@@ -10,7 +10,7 @@ os.system('git pull --quiet 2>/dev/null')
 ranaxd = platform.architecture()[0]
 if ranaxd == '64bit':
  print('\033[1;97m[\033[1;91m+\033[1;91m] \033[1;97mYour Device is 64bit')
- import rana1
+ import Tite1
 elif ranaxd == '32bit':
  print('\033[1;97m[\033[1;91m+\033[1;91m] \033[1;97mYour Devive is 32bit')
- import rana
+ import Tite
