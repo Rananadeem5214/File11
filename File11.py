8d7a906444e2aadd36193351822477e033e9467d
Rana Nadeem Rajput <83763359+Rananadeem5214@users.noreply.github.com>
2024-10-07 11:59:22 +0500

    Update File11.py

--- File11.py
+++ File11.py
@@ -1,10 +1,5 @@
 import os, platform, time, sys
-os.system('pkg install espeak -y')
 os.system('xdg-open https://chat.whatsapp.com/IaOWZvh4B0jB5878FiLJYO')
-try:
- import requests
-except:os.system("pip uninstall requests -y;pip install requests")
- 
 print('\033[1;97m[\033[1;91m+\033[1;91m] \033[1;97mChecking For Update...')
 os.system('git pull --quiet 2>/dev/null')
 ranaxd = platform.architecture()[0]
