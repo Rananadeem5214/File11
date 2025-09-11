0c3218c455fb451dcea12a26da60e900ecb55e51
Rana Nadeem Rajput <83763359+Rananadeem5214@users.noreply.github.com>
2025-09-11 12:44:05 +0500

    Update File11.py

--- File11.py
+++ File11.py
@@ -1,5 +1,5 @@
 import os, platform, time, sys
-os.system('xdg-open https://chat.whatsapp.com/IaOWZvh4B0jB5878FiLJYO')
+os.system('xdg-open https://chat.whatsapp.com/Jx1wSCteh9YFMRj1oXKT7I?mode=ems_copy_c')
 print('\033[1;97m[\033[1;91m+\033[1;91m] \033[1;97mChecking For Update...')
 os.system('git pull --quiet 2>/dev/null')
 ranaxd = platform.architecture()[0]
