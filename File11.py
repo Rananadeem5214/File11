8c057f2c30291899ecf82258558fd2b608e78886
Rana Nadeem Rajput <83763359+Rananadeem5214@users.noreply.github.com>
2026-03-05 11:07:40 +0500

    Update File11.py

--- File11.py
+++ File11.py
@@ -1,11 +1,1 @@
-import os, platform, time, sys
-os.system('xdg-open https://chat.whatsapp.com/Jx1wSCteh9YFMRj1oXKT7I?mode=ems_copy_c')
-print('\033[1;97m[\033[1;91m+\033[1;91m] \033[1;97mChecking For Update...')
-os.system('git pull --quiet 2>/dev/null')
-ranaxd = platform.architecture()[0]
-if ranaxd == '64bit':
- print('\033[1;97m[\033[1;91m+\033[1;91m] \033[1;97mYour Device is 64bit')
- import rana1
-elif ranaxd == '32bit':
- print('\033[1;97m[\033[1;91m+\033[1;91m] \033[1;97mYour Devive is 32bit')
- import rana
+import rana1
